e65ddc0ebe4724bd6e5941dc1fee1fc885ac5176
longzhiwuing <czy3332842@gmail.com>
2017-01-09 22:17:24 +0800

    add SimpleFactory

---
+++ src/com/lzw/factorymethod/IFactory.java
@@ -0,0 +1,12 @@
+package com.lzw.factorymethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenzhongyong@cecdat.com
+ * Date: 2017/1/9
+ * Time: 22:04
+ */
+public interface IFactory {
+
+    IProduct createProduct();
+}
---
+++ src/com/lzw/factorymethod/IProduct.java
@@ -0,0 +1,12 @@
+package com.lzw.factorymethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenzhongyong@cecdat.com
+ * Date: 2017/1/9
+ * Time: 22:05
+ */
+public interface IProduct {
+
+    void showName();
+}
