1002ee7cd65774e10fa96cff2d1857fce8a0d9d8
longzhiwuing <czy3332842@gmail.com>
2017-01-15 11:14:48 +0800

    fix readme

--- src/com/lzw/builder/Config.java
+++ src/com/lzw/builder/Config.java
@@ -32,11 +32,13 @@ public class Config {
                 '}';
     }
 
+    //新增Builder静态类并赋默认值
     public static class Builder {
         private String protocol = "http://";
         private String ip = "127.0.0.1";
         private String port = "80";
 
+        //为每一个属性创建返回自身Builder对象的方法
         public Builder setProtocol(String protocol) {
             this.protocol = protocol;
             return this;
@@ -52,12 +54,14 @@ public class Config {
             return this;
         }
 
+        //新建一个aplly方法，传一个父类引用，赋值构建的参数
         public void apply2Super(Config config) {
             config.protocol = this.protocol;
             config.ip = this.ip;
             config.port = this.port;
         }
 
+        //新建一个build方法，创建一个父类对象，传递给apply方法为这个空对象赋构建出来的参数值，返回这个构建对象即可。
         public Config build(){
             Config config = new Config();
             apply2Super(config);
